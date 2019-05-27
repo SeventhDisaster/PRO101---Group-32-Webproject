0b1363b5883a888659eb3b8cde8808841fb70617
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-05-27 12:40:08 +0200

    Filesystem changes

--- Project/js/dataset.js
+++ Project/js/dataset.js
@@ -2,6 +2,7 @@ let projects = [
     {
         projectName: "PROJECT 1",
         projectDescription: "THIS IS THE FIRST PROJECT",
+        projectTeam: [001,002,003,004,005],
         columns: [
             {
                 columnName: "To-do",
@@ -12,7 +13,7 @@ let projects = [
                         taskDesc: "Description Sample",
                         taskId: 1,
                         taskDue: "7/6/2019",
-                        taskAssignee: "Undefined User",
+                        taskAssignee: [001,002],
                         taskPriority: 1
                     },
                     {
@@ -20,7 +21,7 @@ let projects = [
                         taskDesc: "Description Sample",
                         taskId: 2,
                         taskDue: "4/6/2019",
-                        taskAssignee: "Undefined User",
+                        taskAssignee: [004,003],
                         taskPriority: 2
                     }
                 ]
@@ -34,7 +35,7 @@ let projects = [
                         taskDesc: "Description Sample",
                         taskId: 3,
                         taskDue: "2/6/2019",
-                        taskAssignee: "Undefined User",
+                        taskAssignee: [005],
                         taskPriority: 3
                     }
                 ]
@@ -48,7 +49,7 @@ let projects = [
                         taskDesc: "Description Sample",
                         taskId: 3,
                         taskDue: "2/6/2019",
-                        taskAssignee: "Undefined User",
+                        taskAssignee: [000], // Assigned 000 = No assignment. 
                         taskPriority: 3
                     }
                 ]
@@ -59,11 +60,51 @@ let projects = [
                 tasks: [
         
                 ]
-            }
+            },
+            {
+                columnName: "Etc2",
+                columnId: 5,
+                tasks: [
+        
+                ]
+            },
+            {
+                columnName: "Etc3",
+                columnId: 6,
+                tasks: [
+        
+                ]
+            },
+            {
+                columnName: "Etc4",
+                columnId: 7,
+                tasks: [
+        
+                ]
+            },
         ]
     }
 ]
 
-// Priority 1 = Normal
-// Priority 2 = High
-// Priority 3 = Critical
+let users = [
+    {
+        userId: 001,
+        userName: "Krister Emanuelsen"
+    },
+    {
+        userId: 002,
+        userName: "Andreas Østby"
+    },
+    {
+        userId: 003,
+        userName: "Helene Margel"
+    },
+    {
+        userId: 004,
+        userName: "Simen Nordli Elserud"
+    },
+    {
+        userId: 005,
+        userName: "Zemen Tedros"
+    },
+]
