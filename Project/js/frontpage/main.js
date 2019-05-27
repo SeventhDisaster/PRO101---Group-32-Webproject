80cc4a73e65bacfebbc06177c0d97bffa89b60a7
Andreas Østby <andreas88-ostby@hotmail.com>
2019-05-27 16:29:22 +0200

    Added task fetching and made tasks this week work

--- Project/js/frontpage/main.js
+++ Project/js/frontpage/main.js
@@ -9,11 +9,93 @@ function setup (){
 	console.log(fullnameList);
 
 
-	fillInput(fullnameList, fullname);
+	fillDomList(fullnameList, fullname);
+
+	renderTasks(getProjectsThisWeek());
 
 }
 
-function fillInput(arr, str) {
+function getProjectsThisWeek () {
+
+	let arr = [];
+
+	for (var i = 0; i < projects.length; i++) {
+		for (var j = 0; j < projects[i].columns.length; j++) {
+			var a = projects[i].columns[j];
+			for (var x = 0; x < a.tasks.length; x++) {
+				if(timeBetweenDate(stringToDate(a.tasks[x].due))/24 < 7) {
+					arr.push(a.tasks[x]);
+				}
+			}	
+		}
+	}
+
+	console.log(arr);
+
+	return arr;
+
+}	
+
+function renderTasks (arr) {
+	
+	let taskContainer = document.getElementById("taskContainer");
+	
+	for (var i = 0; i < arr.length; i++) {
+		console.log(timeBetweenDate(stringToDate(arr[i].due)));
+
+		let time;
+		if (stringToDate(arr[i].due).getDate() === new Date().getDate()) {
+			time = "Today";
+		} else if (timeBetweenDate(stringToDate(arr[i].due)) < 24) {
+			time = "Tomorrow"
+		} else {
+			time = Math.ceil(timeBetweenDate(stringToDate(arr[i].due))/24) + "d";
+		}
+	
+		let template = '<td><span class="message">' + arr[i].name + '!</span><span class="dateBox"><span>' + time + '</span></span></td>'
+
+		let dom = document.createElement("tr");
+		dom.innerHTML = template;
+
+		console.log(taskContainer);
+		taskContainer.appendChild(dom);
+
+	}
+
+}
+
+function stringToDate (str) {
+
+	let date = ["", "", ""];
+	let indx = 0;
+
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === "/") {
+			indx++;
+			continue;
+		} 
+		date[indx] += str[i]
+	}
+
+	return new Date(date[2], date[1]-1, date[0]);
+}
+
+function timeBetweenDate (from, to) {
+
+	if (!to) {
+		to = from;
+		from = new Date();
+	}
+
+	let difference = to.getTime() - from.getTime();
+
+	let hours = Math.floor(difference/1000/60/60);
+
+	return hours;
+
+}
+
+function fillDomList(arr, str) {
     for (var i = 0; i < arr.length; i++) {
         arr[i].innerText = str;
     }
