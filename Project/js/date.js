beec0d0894e326e8454bf98ef80e59fc1713af79
Andreas Østby <andreas88-ostby@hotmail.com>
2019-05-28 13:46:18 +0200

    Added date.js

---
+++ Project/js/date.js
@@ -0,0 +1,93 @@
+function stringToDate(str) {
+
+    let date = ["", "", ""];
+    let indx = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === "/") {
+            indx++;
+            continue;
+        }
+        date[indx] += str[i]
+    }
+
+    return new Date(date[2], date[1] - 1, date[0]);
+}
+
+function dateToString(date) {
+
+    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+}
+
+function timeBetweenDate(from, to) {
+
+    if (!to) {
+        to = from;
+        from = new Date();
+    }
+
+    let difference = to.getTime() - from.getTime();
+
+    let hours = Math.floor(difference / 1000 / 60 / 60);
+
+    return hours; // Hours difference
+
+}
+
+function getMonthName (month) {
+
+	switch (month){
+		case 1:
+			return "January"
+		break;
+		case 2:
+			return "February"
+		break;
+		case 3:
+			return "Mars"
+		break;
+		case 4:
+			return "April"
+		break;
+		case 5:
+			return "May"
+		break;
+		case 6:
+			return "June"
+		break;
+		case 7:
+			return "July"
+		break;
+		case 8:
+			return "August"
+		break;
+		case 9:
+			return "September"
+		break;
+		case 10:
+			return "October"
+		break;
+		case 11:
+			return "November"
+		break;
+		case 12:
+			return "December"
+		break;
+	}
+
+}
+
+function formatMonthToString (day, month, year) {
+
+	month = getMonthName(month);
+
+	let str = "";
+	str += day + ". " + month;
+
+	if (year) {
+		str += " " + year
+	}
+
+	return str; // "28. May 2019"
+
+}
