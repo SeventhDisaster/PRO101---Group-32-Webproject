cf879d6c2dd5d63c6a44b127230277f8c34ac4cd
Krister <SeventhDisaster@outlook.com>
2019-06-02 17:04:33 +0200

    Improved task color selection, set name input to chosen color

--- Project/js/project/detailTaskName.js
+++ Project/js/project/detailTaskName.js
@@ -6,6 +6,10 @@ function renderTaskName(task, parent){
     name.setAttribute("placeholder","Type here to set a Task Name");
     parent.appendChild(name);
     name.id = "name"
+
+    if(task.color){
+        name.style.backgroundColor = task.color;
+    }
 }
 
 function getNameValue(){
